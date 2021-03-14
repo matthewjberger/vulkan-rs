00d375610c2e8aabcfe4c4179eb5f2b7b48e1eca
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-03-14 17:38:09 -0600

    Add fullscreen pipeline and shaders

--- src/support/vulkan.rs
+++ src/support/vulkan.rs
@@ -3,6 +3,7 @@ pub use self::{core::*, cube::*, device::*};
 mod core;
 mod cube;
 mod device;
+mod fullscreen;
 
 unsafe fn byte_slice_from<T: Sized>(data: &T) -> &[u8] {
     let data_ptr = (data as *const T) as *const u8;
